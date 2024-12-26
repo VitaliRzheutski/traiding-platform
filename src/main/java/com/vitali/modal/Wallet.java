61b0e709a1dc85a8756d456d93d82bb1f69e193f
Vitali <vitalrzheutsky@gmail.com>
2024-12-26 18:49:37 -0500

    Created Wallet Entity with getters and setters

---
+++ src/main/java/com/vitali/modal/Wallet.java
@@ -0,0 +1,44 @@
+package com.vitali.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) //JPA will automatically generate id's
+    private long id;
+    @OneToOne // one user has only one wallet
+    private User user;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    private BigDecimal balance;
+
+}
