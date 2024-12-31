3cff1c5bf27903a6e51a477aff31a93ad37dd1c8
Vitali <vitalrzheutsky@gmail.com>
2024-12-30 19:35:01 -0500

    Created methods for AssetService

---
+++ src/main/java/com/vitali/service/AssetService.java
@@ -0,0 +1,22 @@
+package com.vitali.service;
+
+import com.vitali.modal.Asset;
+import com.vitali.modal.Coin;
+import com.vitali.modal.User;
+
+import java.util.List;
+
+public interface AssetService {
+
+    Asset createAsset(User user, Coin coin, double quantity);
+    Asset getAssetById(long id) throws Exception;
+    Asset getAssetByUserId(Long userId, long assetId);
+
+    List<Asset> getUsersAssets(Long userId);
+
+    Asset updateAsset(Long assetId, double quantity) throws Exception;
+
+    Asset findAssetByUserIdAndCoinId(Long userId, String coinId);
+    void deleteAsset(Long assetIdl);
+
+}
