1c5856911e247df69dc7d2f7e5b39b9fdb61bc80
Vitali <vitalrzheutsky@gmail.com>
2025-01-07 13:41:35 -0500

    Created Withdrawal class with fields and getters and setters

---
+++ src/main/java/com/vitali/modal/Withdrawal.java
@@ -0,0 +1,66 @@
+package com.vitali.modal;
+
+import com.vitali.domain.WithdrawalStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Withdrawal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private WithdrawalStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public WithdrawalStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(WithdrawalStatus status) {
+        this.status = status;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    private Long amount;
+
+    @ManyToOne
+    private User user;
+
+    private LocalDateTime date = LocalDateTime.now();
+
+}
