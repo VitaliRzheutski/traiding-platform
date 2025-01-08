30f12adb0b23a18fb5221012c41312fb1185c6aa
Vitali <vitalrzheutsky@gmail.com>
2025-01-08 10:54:25 -0500

    Created WatchList class with fields and getter/setters

---
+++ src/main/java/com/vitali/modal/WatchList.java
@@ -0,0 +1,47 @@
+package com.vitali.modal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class WatchList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Coin> getCoins() {
+        return coins;
+    }
+
+    public void setCoins(List<Coin> coins) {
+        this.coins = coins;
+    }
+
+    @OneToOne
+    private User user;
+
+    @ManyToMany
+    private List<Coin> coins = new ArrayList<>();
+
+}
