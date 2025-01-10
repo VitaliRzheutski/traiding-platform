df322bb1330bf206809eedf450b77c0f68a55293
Vitali <vitalrzheutsky@gmail.com>
2025-01-09 19:28:26 -0500

    Defined and implementes payment service methods

---
+++ src/main/java/com/vitali/service/PaymentService.java
@@ -0,0 +1,20 @@
+package com.vitali.service;
+
+import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
+import com.vitali.domain.PaymentMethod;
+import com.vitali.modal.PaymentOrder;
+import com.vitali.modal.User;
+import com.vitali.response.PaymentResponse;
+
+public interface PaymentService {
+
+    PaymentOrder createOrder(User user, Long amount, PaymentMethod paymentMethod);
+    PaymentOrder getPaymentOrderById(Long id) throws Throwable;
+    Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String paymentId) throws RazorpayException;
+
+    PaymentResponse createRazorpayPaymentLink(User user, Long amount);
+    PaymentResponse stripePaymentLink(User user, Long amount, Long orderId) throws StripeException;
+
+
+}
